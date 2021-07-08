1adbc8ae271b4c342db5ed1f8237144f686e3f23
DAHLIACHOI <cyj000424@naver.com>
2021-07-08 15:27:03 +0900

    [section3] DI, 스프링컨테이너 사용

--- src/test/java/hello/core/member/MemberServiceTest.java
+++ src/test/java/hello/core/member/MemberServiceTest.java
@@ -1,12 +1,22 @@
 package hello.core.member;
 
+import hello.core.AppConfig;
 import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 public class MemberServiceTest {
 
-    MemberService memberService = new MemberServiceImpl();
+    MemberService memberService;
+
+    @BeforeEach
+    // 실행하기 전에 무조건 동작해야됨
+    public void beforeEach(){
+        AppConfig appConfig = new AppConfig();
+        memberService = appConfig.memberService();
+
+    }
 
     @Test
     void join(){
