933d908874610693bef378f93a9d03ea98933b35
DAHLIACHOI <cyj000424@naver.com>
2021-07-15 22:21:11 +0900

    [section7] lombok

--- src/main/java/hello/core/order/OrderServiceImpl.java
+++ src/main/java/hello/core/order/OrderServiceImpl.java
@@ -7,26 +7,16 @@ import hello.core.member.Member;
 import hello.core.member.MemberRepository;
 import hello.core.member.MemberService;
 import hello.core.member.MemoryMemberRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
+@RequiredArgsConstructor // 자동으로 생성자를 만들어줌
 public class OrderServiceImpl implements OrderService {
 
-    private MemberRepository memberRepository;
-    private DiscountPolicy discountPolicy; // DIP지키고 있음, 순전히 인터페이스에만 의존
-
-    //@Autowired //생성자 하나일 때는 수정가능
-    public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
-        this.memberRepository = memberRepository;
-        this.discountPolicy = discountPolicy;
-    }
-
-    @Autowired
-    public void init(MemberRepository memberRepository, DiscountPolicy discountPolicy){
-        this.memberRepository = memberRepository;
-        this.discountPolicy = discountPolicy;
-    }
+    private final MemberRepository memberRepository;
+    private final DiscountPolicy discountPolicy; // DIP지키고 있음, 순전히 인터페이스에만 의존
 
     @Override
     public Order createOrder(Long memberId, String itemName, int itemPrice) {
